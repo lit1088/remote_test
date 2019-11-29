0be253e47a8f637371fb75e75d254a627f5a7e71
lit1088 <9921025530@qq.com>
2019-11-29 20:27:22 +0800

    提交js

---
+++ test2.js
@@ -0,0 +1,6 @@
+var qq = "992102530";
+app.startActivity({ 
+    action: "android.intent.action.VIEW", 
+    data:"mqq://im/chat?chat_type=wpa&version=1&src_type=web&uin=" + qq, 
+    packageName: "com.tencent.mobileqq", 
+});
